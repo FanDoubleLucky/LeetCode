277294d68563b73a0c0eb83b4c5613debb303e6a
FanDoubleLucky <1398328478@qq.com>
2018-09-03 10:42:05 +0800

    2-sum

--- src/K_Sum/sum_2.java
+++ src/K_Sum/sum_2.java
@@ -1,4 +1,24 @@
 package K_Sum;
 
-public class sum_2 {
+import java.util.HashMap;
+import java.util.Map;
+
+class sum_2 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        int[] res = new int[2];
+        int N = nums.length;
+        for(int i=0;i<N;i++){
+            map.put(nums[i],i);
+        }
+        for(int i=0;i<N;i++){
+            int component = target - nums[i];
+            if(map.containsKey(component)&&map.get(component)!=i){
+                res[0] = i;
+                res[1] = map.get(component);
+                return res;
+            }
+        }
+        return res;
+    }
 }
