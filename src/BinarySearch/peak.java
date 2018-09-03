5c3c8e5ad3eb3506632839ffef53e027d2e39f2e
FanDoubleLucky <1398328478@qq.com>
2018-09-03 21:31:38 +0800

    BS_peak_find

---
+++ src/BinarySearch/peak.java
@@ -0,0 +1,20 @@
+package BinarySearch;
+class Solution {
+    public int peakIndexInMountainArray(int[] A) {
+        int N = A.length;
+        int lo = 0,hi = N-1;
+        while(true){
+            if(hi-lo<2){return lo+(hi-lo)/2;}
+            int mid = lo+(hi-lo)/2;
+            if(A[mid-1]<A[mid]&&A[mid]<A[mid+1]){
+                lo = mid;
+            }
+            else if(A[mid-1]>A[mid]&&A[mid]>A[mid+1]){
+                hi = mid;
+            }
+            else{
+                return mid;
+            }
+        }
+    }
+}
