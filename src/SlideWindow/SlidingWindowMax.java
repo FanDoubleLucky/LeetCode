9f61e8aa28e8cbde574f8656a227d05efcb96a5c
FanDoubleLucky <1398328478@qq.com>
2019-09-23 23:47:54 +0800

    Sliding window max

---
+++ src/SlideWindow/SlidingWindowMax.java
@@ -0,0 +1,15 @@
+package SlideWindow;
+
+/*
+[1 9 8 6 7 3 5 2], k=4,输出:9, 9, 8, 7, 7
+ */
+
+public class SlidingWindowMax {
+    public static void SWMAX(int[] arr, int k){
+        if(arr==null){return;}
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
