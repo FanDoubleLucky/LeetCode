ddbb01a932a7c4bdca5880b200199813d898603a
FanDoubleLucky <1398328478@qq.com>
2019-09-24 19:24:04 +0800

    TreeNode Class

--- src/Tree/LongestUnivaluePath.java
+++ src/Tree/LongestUnivaluePath.java
@@ -2,14 +2,27 @@ package Tree;
 
 public class LongestUnivaluePath {
 
-    public int longestUnivaluePath(TreeNode root) {
+    static int len = 0;
+    public static int longestUnivaluePath(TreeNode root) {
+        if(root==null){return 0;}
 
-        return 0;
+        int count = dfs(root.val, root.left)+dfs(root.val, root.right)+1;
+        len = Math.max(len, count);
+        return len-1;
+    }
+
+    static int dfs(int val, TreeNode node){
+        if (node==null){return 0;}
+        int left = dfs(node.val, node.left);
+        int right = dfs(node.val, node.right);
+        len = Math.max(len, left+1+right);
+        if(node.val!=val){return 0;}
+        return Math.max(left, right)+1;
     }
 
     public static void main(String[] args) {
         TreeNode root = TreeNode.Load3Level(new int[]{5,4,5,1,1,Integer.MAX_VALUE,5});
-        System.out.println(root.toString());
+        System.out.println(longestUnivaluePath(root));
     }
 
 }
